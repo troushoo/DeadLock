bf8f3f5e946f5d0fd226d4177afb9ce8247676d5
tr <troushoo@gmail.com>
2017-04-09 23:34:23 +0900

    DeadLock

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DeadLockSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj1 = new object();
+            object obj2 = new object();
+            
+            Task task1 = Task.Run(
+                () =>
+                {
+                    lock (obj1)
+                    {
+                        System.Threading.Thread.Sleep(2000);
+                        Console.WriteLine("Trying to get obj2 from task1");
+
+                        lock (obj2)
+                        {
+                            Console.WriteLine("task1 get obj2");
+                        }
+                    }
+
+                }
+                );
+
+            Task task2 = Task.Run(
+                () =>
+                {
+                    lock (obj2)
+                    {
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine("Trying to get obj1 from task2");
+
+                        lock (obj1)
+                        {
+                            Console.WriteLine("task2 get obj1");
+                        }
+                    }
+
+                }
+                );
+
+
+            Console.ReadLine();
+        }
+    }
+}
